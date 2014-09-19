ecebcbadc8aceea25fd5b5ea3bb3df9eb56b6068
marylinu <s.marylinu@gmail.com>
2014-09-19 16:16:31 -0400

    Updated Audio class

--- LiquiZ/src/org/adastraeducation/quiz/Audio.java
+++ LiquiZ/src/org/adastraeducation/quiz/Audio.java
@@ -1,29 +1,26 @@
 package org.adastraeducation.quiz;
 
-public class Audio implements Displayables {
-	private String audioQuestion;
+public class Audio implements Displayable {
+	private String audio;
 	private String type;
-	private String [] answer;
 	
-	public Audio(){
-		answer = null;		
+	public Audio(String audio){
+		this.audio= audio;		
 	}
 	//<source src="horse.ogg" type="audio/ogg">
 	//<source src="horse.mp3" type="audio/mpeg">
-	public void WriteHTML(StringBuilder b){
+	public void writeHTML(StringBuilder b){
 		b.append("<audio controls>");
-		b.append("<source src=" + audioQuestion + "type ="+ type + ">");
-		b.append("<select>");
-		b.append("<input type=\"radio\"" + answer + "<br>");
+		b.append("<source src=" + audio + "type ="+ type + ">");
 		b.append("</audio>");
 	}
 	// to do : how to represent image in JavaScript
-	public void WriteJS(StringBuilder b){
-		b.append("new Audio(1, \"" + answer + "\")");
+	public void writeJS(StringBuilder b){
+		b.append("new Audio(1, \"" + audio + "\")");
 	}
 	
-	public void WriteXML(StringBuilder b) {
-		b.append("<A correct=\"" + answer + "\"> </A>");
+	public void writeXML(StringBuilder b) {
+		b.append("<A correct=\"" + audio + "\"> </A>");
 	}
 
 }
