b16d000bfb0a11e284906b6af74eb6b586778d33
hydrodog <dkruger@WINDOWS-KKJL8IJ>
2014-09-20 19:25:34 -0400

    Putting in cleaned version of Image. Every image has a name which is a string.

--- LiquiZ/src/org/adastraeducation/liquiz/Image.java
+++ LiquiZ/src/org/adastraeducation/liquiz/Image.java
@@ -3,18 +3,18 @@ package org.adastraeducation.liquiz;
 public class Image implements Displayable {
 	private String width;
 	private String height;
-	private String [] name;
+	private String name;
 	
 	public Image(){
 		name = null;		
 	}
 	//TODO: Add Image constructor here!!
 	public Image(String image){
-		this.image= image;
+		this.name= image;
 	}
 	
 	public Image(String image, String width, String height){
-		this.image= image;
+		this.name = image;
 		this.height= height;
 		this.width= width;
 	}
