8163e7525119dafce447f9a9370bca5bf910a6b0
marylinu <s.marylinu@gmail.com>
2014-09-26 12:14:12 -0400

    Update StdChoice.java

--- LiquiZ/src/org/adastraeducation/liquiz/StdChoice.java
+++ LiquiZ/src/org/adastraeducation/liquiz/StdChoice.java
@@ -16,7 +16,44 @@ public class StdChoice {
 		this.answers =  opinion.get(name);
 	}
 
-	
+
+	public StdChoice(String name, int rightAns) {
+		this.name = name;
+		hashMapStdChoice();
+		this.answers =  opinion.get(name);
+		setAnswer(rightAns);
+	}
+
+	 public StdChoice(String name, int[] rightAns) {
+    	this.name = name;
+		hashMapStdChoice();
+		this.answers =  opinion.get(name);
+		setmultiAnswer(rightAns);	}
+
+
+
+	// this method will set the right answer to multidropdown and multiradio
+	public void setAnswer(int rightAns) {
+		for (Answer ans: answers){
+			if (ans.getIndex()== rightAns){
+				ans.setCorrect(true);
+				break;
+			}
+		}
+	}
+	// this method will set the multiple right answers 
+	private void setmultiAnswer(int[] rightAns) {
+		for (Answer ans: answers){
+			for (int i :rightAns){
+				if (ans.getIndex()== i){
+					ans.setCorrect(true);
+					break;
+				}
+			}
+		}		
+	}
+
+
 	private void hashMapStdChoice() {
 		opinion = new HashMap<String, Answer[]>();
 
@@ -24,15 +61,53 @@ public class StdChoice {
 			    new Answer("Agree"), new Answer("No Opinion"),
 			    new Answer("Disagree"),new Answer("Strongly Disagree")
 			  });
+		opinion.put("Complexity",  new Answer[] {
+			    new Answer(1,"O(1)"),new Answer(2,"O(n)"),new Answer(3,"O(n^2)"),
+			    new Answer(4,"O(n^3)"),new Answer(5,"O(n logn)")
+			  });
+		opinion.put("Colors",  new Answer[] {
+			    new Answer(1,"Blue"),new Answer(2,"Red"),new Answer(3,"Green"),
+			    new Answer(4,"Orange"),new Answer(5,"Purple")
+			  });
+		opinion.put("Insects",  new Answer[] {
+			    new Answer(1,"Dog"),new Answer(2,"Cat"),new Answer(3,"Grasshopper"),
+			    new Answer(4,"Butterfly"),new Answer(5,"Ladybug")
+			  });
 	}
 	
-	public void writeHTML(StringBuilder b) {
-		for (int i = 0; i < answers.length; i++)
+	public void writeHTMLDropdown(StringBuilder b){
+		b.append("<select>");
+		for (int i = 0; i < answers.length; i++) {
+			b.append("<option value='" + answers[i].getAnswer() + "'> "
+					+ answers[i].getAnswer() + " </option> ");
+		}
+		b.append("</select>");
+		b.append("</br>");
+		
+	}
+	public void writeHTMLRadio(StringBuilder b) {
+		
+		for (int i = 0; i < answers.length; i++){
 			b.append("<input type=\"radio\" name='").append(name).append("'>")
-					.append(answers[i].getAnswer()).append("<br>");
+					.append(answers[i].getAnswer()).append("<br>");}
+		
 	}
 
+	
+
+	public void writeHTMLMultiSelection(StringBuilder b) {
+		b.append("<select multiple>");
+		for (int i = 0; i < answers.length; i++){
+			b.append("<option value= '" + answers[i].getAnswer() + "'>"+ answers[i].getAnswer() +"  </option> ");
+		 }
+		b.append("</select>");
+		b.append("</br>");
+		// TODO Auto-generated method stub
+		
+	}
 	public void writeXML(StringBuilder b) {
 		for (int i = 0; i < answers.length; i++)
 			answers[i].writeXML(b);
 	}
+
+}
