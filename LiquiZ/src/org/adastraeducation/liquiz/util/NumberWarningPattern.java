66db24b5e6f9c846aabf8c0e04281d894835ece2
banboon <wangyzch@163.com>
2014-09-29 21:41:41 -0400

    I add a func.js in the /web/assets/ folders. This is for javascript. If Stehpen has its own file, I can merge this file with his.

---
+++ LiquiZ/src/org/adastraeducation/liquiz/util/NumberWarningPattern.java
@@ -0,0 +1,21 @@
+package org.adastraeducation.liquiz.util;
+
+public class NumberWarningPattern {
+	
+	private  int limit;
+	
+	public NumberWarningPattern(int n){
+		this.limit=n;
+	}
+
+	public int getLimit() {
+		return limit;
+	}
+
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
+	
+	
+
+}
---
+++ LiquiZ/web/assets/func.js
@@ -0,0 +1,18 @@
+/*
+ *This is file is for javascript
+ */
+ 
+ 
+/*
+ *warning pattern
+ */
+function showWarning(id,n) {
+    var x = $("#"+id).val();
+    $("#FW"+id).empty();
+    
+    eval("var pattern = /[0-9]+.[0-9]{"+n+"}/;");
+  
+    if (x.match(pattern)==null){
+        $("#FW"+id).append("Wrong input!");
+    } 
+}
