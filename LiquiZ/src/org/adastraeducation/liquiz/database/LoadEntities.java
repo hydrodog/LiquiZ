6deaabdbd123d9776df9957ae729130989b2beda
Jinnie <yijkan@bergen.org>
2014-10-22 14:55:41 -0400

    Creating img / aud / vid

--- LiquiZ/src/org/adastraeducation/liquiz/database/LoadEntities.java
+++ LiquiZ/src/org/adastraeducation/liquiz/database/LoadEntities.java
@@ -8,6 +8,7 @@ public class LoadEntities {
 		Connection conn = null;
 		try {
 			conn = DatabaseMgr.getConnection();
+			
 			PreparedStatement p = conn.prepareStatement("SELECT Element, Type FROM DispElSeq WHERE DispEl=? ORDER BY Sequence ASC");
 			p.setInt(1, DispElID);
 			
@@ -20,11 +21,11 @@ public class LoadEntities {
 				}
 				org.adastraeducation.liquiz.Text d = new org.adastraeducation.liquiz.Text(sb.toString());
 			} else if (type.equals("img")) {
-				
+				org.adastraeducation.liquiz.Image d = new org.adastraeducation.liquiz.Image(rs.getString("Element"));
 			} else if (type.equals("aud")) {
-				
+				org.adastraeducation.liquiz.Audio d = new org.adastraeducation.liquiz.Audio(rs.getString("Element"));
 			} else if (type.equals("vid")) {
-				
+				org.adastraeducation.liquiz.Video d = new org.adastraeducation.liquiz.Video(rs.getString("Element"));
 			}
 
 			rs.close();
